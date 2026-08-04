e015aaae2b8fcfa2535d89afe2670ef79248f044
JULIA PURCINELLI MELO COSTA <julia.pmcosta@senacsp.edu.br>
2026-08-04 14:01:07 -0300

    Quarto commit com Aplicação em BD

--- View/Excluir.php
+++ View/Excluir.php
@@ -1,17 +1,26 @@
 <?php
 
  namespace Projeto\ti23t\view;
+ require_once('../DAO/excluir.php'); // agora estamos conectando com o Banco de Dados 
+  require_once('../DAO/conexao.php');
   require_once('../model\cliente.php');
   require_once('../control\ClienteControl.php');
   use Projeto\ti23t\model\Cliente;
   use Projeto\ti23t\control\Control;
+    use Projeto\ti23t\DAO\Conexao;
+  use Projeto\ti23t\DAO\Excluir;
 
 
  //iniciar a sessao 
-  session_start();
-  $clienteRecuperado= $_SESSION["cliente"];
-  $controle= new Control($clienteRecuperado);// para acessar os metodos de atualizaçao 
-  $resultado=0;//instanciar ,da o valor inicial 
+  //session_start();
+  //$clienteRecuperado= $_SESSION["cliente"];
+  //$controle= new Control($clienteRecuperado);// para acessar os metodos de atualizaçao 
+//  $resultado=0;//instanciar ,da o valor inicial 
+
+// instanciar 
+  $conexao= new Conexao();
+  $excluir= new Excluir();
+  $resultado="";
   
 ?>
 
@@ -24,26 +33,38 @@
     <title>Excluir Cliente</title>
 </head>
 <body>
-    <h1>Exluir Cliente </h1>
-      <?php echo "Deseja excluir o cliente ".$clienteRecuperado->código."?"?>
- 
+    <h1>Excluir Cliente </h1>
+    
     <form method="POST">
-       <button type="submit">Excluir
+
+    
+        <label>Codigo:</label>
+        <input type="number" name="codigo" id="codigo">
+        <br> <br> 
+
+        <button type="submit"> Excluir 
         <?php 
-        $resultado=$controle->excluir();
+        
+        
+        //$resultado=$controle->excluir();
+         
+        $resultado=$excluir-> excluirCliente($conexao,$_POST['codigo']);
+
         ?>
        </button>
     </form>
 
     <?php 
-    if($resultado==1)
-    {
-       echo " excluido com sucesso";
-    }
-    else
-    {
-    echo"confirme a exlusão";
-    }
+    //if($resultado==1)
+   // {
+       //echo " excluido com sucesso";
+   // }
+    //else
+    //{
+    //echo"confirme a exlusão";
+    //}
+
+    echo $resultado;
     
     ?>
     <br>
